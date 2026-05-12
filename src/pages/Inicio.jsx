fe06dee2e385c671084c2ed095b6e7ca45e3e1ff
Juan David <juanprogamadorfullstack@gmail.com>
2026-05-11 20:38:51 -0500

    pequeño arreglo

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,6 +2,7 @@ const Inicio = () => {
     return (
         <div>
             <h1>Pagina de inicio</h1>
+            <p>Todos nuestros productos en descuento</p>
         </div>
     );
 }
--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -3,12 +3,12 @@ import { Outlet, Link } from 'react-router-dom';
 const Productos = () => {
     return (
         <div classNmae="productos-container">
-            <h1>Nuestra Tienda</h1>
-            <nav>
-                <Link to='hombre'>Seccion Hombre</Link>
-                <Link to='mujer'>Seccion Mujer</Link>
+            <h1 classNmae="productos-titulo">Nuestra Tienda</h1>
+            <nav className="productos-nav">
+                <Link to="hombre" className="productos-link">Sección Hombre</Link>
+                <Link to="mujer" className="productos-link">Sección Mujer</Link>
             </nav>
-            <div>
+            <div className="productos-contenido">
                 <Outlet />
             </div>
         </div>
